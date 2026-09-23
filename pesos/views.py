a162a17f7e4a47330eaf457644507c04a6dec7ce
Azure DevOps Pipeline <pipeline@azuredevops.com>
2026-09-23 17:35:28 -0300

    renomeia pastas do projeto para academia/pesos

--- pesos/views.py
+++ pesos/views.py
@@ -2,17 +2,18 @@ from django.shortcuts import render
 from .models import Livro
 from .forms import LivroForm
 
+
 # Create your views here.
 def home(request):
     livros = Livro.objects.all()
 
     if request.method == 'POST':
         form = LivroForm(request.POST)
+
         if form.is_valid():
             form.save()
 
     else:
-        livros = Livro.objects.all()
         form = LivroForm()
 
     return render(request, 'index.html', {'form':form,'livros': livros})
