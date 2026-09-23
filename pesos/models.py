b0e839d26f9717841fa9d9f143bc081d8e8e91e0
Azure DevOps Pipeline <pipeline@azuredevops.com>
2026-09-23 17:40:29 -0300

    troca o model Livro pelo model Peso

--- pesos/models.py
+++ pesos/models.py
@@ -1,26 +1,21 @@
 from django.db import models
 
-class Livro(models.Model):
-    class Classificacao(models.TextChoices):
-        GENERALIDADES = '000', '000 – Generalidades e Informação'
-        FILOSOFIA = '100', '100 – Filosofia e Psicologia'
-        RELIGIAO = '200', '200 – Religião e Teologia'
-        CIENCIAS_SOCIAIS = '300', '300 – Ciências Sociais e Direito'
-        LINGUISTICA = '400', '400 – Linguística e Idiomas'
-        CIENCIAS_PURAS = '500', '500 – Ciências Puras'
-        CIENCIAS_APLICADAS = '600', '600 – Ciências Aplicadas'
-        ARTES = '700', '700 – Artes e Recreação'
-        LITERATURA = '800', '800 – Literatura'
-        HISTORIA = '900', '900 – História e Geografia'
+class Peso(models.Model):
+    class Tipo(models.TextChoices):
+        HALTER = 'HAL', 'Halter'
+        ANILHA = 'ANI', 'Anilha'
+        BARRA = 'BAR', 'Barra'
+        KETTLEBELL = 'KET', 'Kettlebell'
+        CANELEIRA = 'CAN', 'Caneleira'
 
     nome = models.CharField(max_length=100)
-    autor = models.CharField(max_length=100)
+    marca = models.CharField(max_length=100)
     valor = models.FloatField()
     date = models.DateField()
-    classificacao = models.CharField(
+    tipo = models.CharField(
         max_length=3,
-        choices=Classificacao.choices,
-        default=Classificacao.GENERALIDADES,
+        choices=Tipo.choices,
+        default=Tipo.HALTER,
     )
 
     def __str__(self):
