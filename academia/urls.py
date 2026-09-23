714c8a524c131050b5fca377fc667db64932e3c2
Azure DevOps Pipeline <pipeline@azuredevops.com>
2026-09-23 17:35:44 -0300

    aponta as configuracoes para o projeto academia

--- academia/urls.py
+++ academia/urls.py
@@ -1,5 +1,5 @@
 """
-URL configuration for biblioteca project.
+URL configuration for academia project.
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/6.1/topics/http/urls/
