cf2c74d88eea6659e96c31816e94b5ff3e11ee67
Felipe Sodre <felipesodre01e@gmail.com>
2026-09-23 19:41:31 -0300

    valida que o valor do produto e maior que zero

--- produtos/forms.py
+++ produtos/forms.py
@@ -5,3 +5,11 @@ class ProdutoForm(forms.ModelForm):
     class Meta:
         model = Produto
         fields = ['nome','valor','marca','date', 'tipo']
+
+    def clean_valor(self):
+        valor = self.cleaned_data.get('valor')
+
+        if valor <= 0:
+            raise forms.ValidationError("O valor do produto deve ser maior que zero.")
+
+        return valor
