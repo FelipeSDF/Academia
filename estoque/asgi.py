0cfbaadfe17497f5a1765c68b8da7b751ca7933c
Azure DevOps Pipeline <pipeline@azuredevops.com>
2026-09-23 18:44:28 -0300

    aponta as configuracoes para o projeto estoque

--- estoque/asgi.py
+++ estoque/asgi.py
@@ -1,5 +1,5 @@
 """
-ASGI config for academia project.
+ASGI config for estoque project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'academia.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'estoque.settings')
 
 application = get_asgi_application()
--- estoque/urls.py
+++ estoque/urls.py
@@ -1,5 +1,5 @@
 """
-URL configuration for academia project.
+URL configuration for estoque project.
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/6.1/topics/http/urls/
@@ -16,8 +16,8 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-from pesos import urls as pesosViews
+from produtos import urls as produtosViews
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-] + pesosViews.urls
+] + produtosViews.urls
